15b14408bbd8d81b497c1e06bf05ac568c791530
R0lton <108031948+R0lton@users.noreply.github.com>
2024-07-11 16:29:15 +0300

    commit changes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from bson import ObjectId
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_file
 from pymongo import MongoClient
 
 HOST = "localhost"
@@ -11,15 +12,17 @@ client = MongoClient(HOST, PORT)
 db = client['admin']
 reqs = db['requests']
 offices = db['work_main_plan']
-
+exceptions = db['work_plan_exceptions']
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    reqs_lst = reqs.find({"status": "Новый"})
+    today = datetime.now().strftime('%Y-%m-%d')
+    reqs_lst = reqs.find({"status": "Новый", 'date': {'$gte': today}})
     offices_lst = offices.find()
-    return render_template('index.html', requests=reqs_lst, offices=offices_lst)
+    offices_lst1 = offices.find()
+    return render_template('index.html', requests=reqs_lst, offices=offices_lst, offices1=offices_lst1)
 
 
 @app.template_filter('format_date')
@@ -53,33 +56,18 @@ def get_office_schedule(office_name):
     return jsonify({})
 
 
+@app.route('/get_exceptions/<office_name>/<selected_date>', methods=['GET'])
+def get_exceptions(office_name, selected_date):
+    excs = exceptions.find_one({'office': office_name, 'date': selected_date})
+    if excs:
+        return jsonify(excs['exc'])
+    return jsonify({})
+
+
 @app.route('/get_records/<office_name>/<date>', methods=['GET'])
 def get_records(office_name, date):
     curr_reqs = reqs.find({'office': office_name, 'date': date})
     return jsonify([req['time'] for req in curr_reqs])
-
-
-@app.route('/edit/<req_id>')
-def show_update_form(req_id):
-    req = reqs.find_one({'_id': ObjectId(req_id)})
-    offices_lst = offices.find()
-    return render_template('edit.html', req=req, offices=offices_lst)
-
-
-@app.route('/edit/<req_id>', methods=['POST'])
-def update_req(req_id):
-    updated_req = {
-        'surname': request.form.get('surname'),
-        'name': request.form.get('name'),
-        'fathername': request.form.get('fathername'),
-        'contact_phone': request.form.get('phone_number'),
-        'office': request.form.get('office'),
-        'date': request.form.get('date'),
-        'time': request.form.get('time'),
-        'status': request.form.get('status')
-    }
-    reqs.update_one({'_id': ObjectId(req_id)}, {'$set': updated_req})
-    return redirect(url_for('home_page'))
 
 
 @app.route('/update_status/<req_id>', methods=['POST'])
@@ -91,6 +79,28 @@ def update_status(req_id):
         return jsonify({'success': True})
     else:
         return jsonify({'success': False, 'error': 'Record not found or status unchanged'}), 404
+
+
+@app.route('/export_to_excel', methods=['GET'])
+def export_to_excel():
+    cursor = reqs.find()
+    df = pd.DataFrame(list(cursor))
+    file_name = 'all_records.xlsx'
+    writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Записи', index=False)
+    writer.close()
+
+    return send_file(file_name, as_attachment=True)
+
+
+@app.route('/main_work')
+def main_work():
+    return render_template('main_work_plan.html')
+
+
+@app.route('/exceptions')
+def main_work_plan():
+    return render_template('exceptions.html')
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,13 +1,3 @@
-document.querySelector('.open-form').addEventListener('click', () => {
-    document.querySelector('.form--form').classList.remove('hidden');
-});
-
-
-document.querySelector('.close-form').addEventListener('click', () => {
-    document.querySelector('.form--form').classList.add('hidden');
-});
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const officeSelect = document.getElementById('office');
     const dateInput = document.getElementById('date');
@@ -40,12 +30,14 @@ document.addEventListener('DOMContentLoaded', function () {
         ];
     }
 
+
     function updateAvailableTimes() {
         const selectedOffice = officeSelect.value;
         const selectedDate = new Date(dateInput.value);
         const dayOfWeek = selectedDate.toLocaleString('en-EN', {weekday: 'short'}).toLowerCase();
         const url = `/get_office_schedule/${selectedOffice}`;
         const reqsUrl = `/get_records/${selectedOffice}/${dateInput.value}`;
+        const excsUrl = `/get_exceptions/${selectedOffice}/${dateInput.value}`;
 
         fetch(url)
             .then(response => response.json())
@@ -57,21 +49,31 @@ document.addEventListener('DOMContentLoaded', function () {
                     fetch(reqsUrl)
                         .then(response => response.json())
                         .then(records => {
-                            const startHour = parseInt(officeHours.start.split(':')[0]);
-                            const endHour = parseInt(officeHours.end.split(':')[0]);
-                            const breakTime = officeHours.break ? officeHours.break.split('-') : [];
-                            for (let hour = startHour; hour < endHour; hour++) {
-                                if (breakTime.length && hour >= parseInt(breakTime[0].split(':')[0]) && hour < parseInt(breakTime[1].split(':')[0])) {
-                                    continue;
-                                }
-                                const timeString = `${hour}:00`;
-                                if (!records.includes(timeString)) {
-                                    const option = document.createElement('option');
-                                    option.value = timeString;
-                                    option.text = timeString;
-                                    timeSelect.appendChild(option);
-                                }
-                            }
+                            fetch(excsUrl)
+                                .then(response => response.json())
+                                .then(excData => {
+
+                                    let startHour = parseInt(officeHours.start.split(':')[0]);
+                                    let endHour = parseInt(officeHours.end.split(':')[0]);
+                                    const isActiveAtypical = document.querySelector('#atypical').checked;
+                                    if (excData['start_time'] && excData['end_time'] && isActiveAtypical) {
+                                        startHour = parseInt(excData['start_time'].split(':')[0]);
+                                        endHour = parseInt(excData['end_time'].split(':')[0]);
+                                    }
+                                    const breakTime = officeHours.break ? officeHours.break.split('-') : [];
+                                    for (let hour = startHour; hour < endHour; hour++) {
+                                        if (breakTime.length && hour >= parseInt(breakTime[0].split(':')[0]) && hour < parseInt(breakTime[1].split(':')[0])) {
+                                            continue;
+                                        }
+                                        const timeString = `${hour}:00`;
+                                        if (!records.includes(timeString)) {
+                                            const option = document.createElement('option');
+                                            option.value = timeString;
+                                            option.text = timeString;
+                                            timeSelect.appendChild(option);
+                                        }
+                                    }
+                                });
                         });
                 }
             });
@@ -126,3 +128,102 @@ document.addEventListener('DOMContentLoaded', function () {
 
     updateAvailableDates();
 });
+
+document.querySelector('.open-form').addEventListener('click', () => {
+    document.querySelector('.form--form').classList.remove('hidden');
+});
+
+
+document.querySelector('.close-form').addEventListener('click', () => {
+    document.querySelector('.form--form').classList.add('hidden');
+});
+
+function sortTable(columnIndex) {
+    const table = document.querySelector('.requests-table');
+    const tbody = table.querySelector('tbody');
+    const rows = Array.from(tbody.rows);
+
+    rows.sort((rowA, rowB) => {
+        const cellA = rowA.cells[columnIndex].textContent.trim();
+        const cellB = rowB.cells[columnIndex].textContent.trim();
+
+        if (columnIndex === 5) { // Сортировка по дате
+            const dateA = new Date(cellA.split('.').reverse().join('-'));
+            const dateB = new Date(cellB.split('.').reverse().join('-'));
+            return dateA - dateB;
+        } else if (columnIndex === 6) { // Сортировка по времени
+            const dateA = new Date(rowA.cells[5].textContent.trim().split('.').reverse().join('-'));
+            const dateB = new Date(rowB.cells[5].textContent.trim().split('.').reverse().join('-'));
+            if (dateA - dateB === 0) { // Если даты одинаковые, сортируем по времени
+                const timeA = rowA.cells[6].textContent.trim();
+                const timeB = rowB.cells[6].textContent.trim();
+                return timeA.localeCompare(timeB);
+            } else {
+                return dateA - dateB;
+            }
+        } else {
+            return cellA.localeCompare(cellB);
+        }
+    });
+
+    // Перерисовка таблицы в новом порядке с учетом заголовка
+    rows.forEach(row => {
+        tbody.appendChild(row); // Переносим строки в порядке сортировки
+    });
+}
+
+const filterOfficeSelect = document.getElementById('filter-office');
+filterOfficeSelect.addEventListener('change', function () {
+    const selectedOffice = this.value.toLowerCase();
+
+    const table = document.querySelector('.requests-table');
+    const rows = Array.from(table.rows);
+    console.log(rows[0].cells);
+    rows.forEach(row => {
+        const officeCell = row.cells[4].textContent.trim().toLowerCase();
+        if (selectedOffice === '' || officeCell === selectedOffice) {
+            row.style.display = '';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+    rows[0].style.display = '';
+});
+
+
+function exportToExcel() {
+    const table = document.querySelector('.requests-table');
+    const rows = Array.from(table.querySelectorAll('tbody tr'));
+    rows.shift();
+
+     const data = rows.map(row => {
+        // Проверяем стиль display у строки
+        if (window.getComputedStyle(row).display !== 'none') {
+            // Формируем массив данных для текущей видимой строки
+            const rowData = Array.from(row.cells).map(cell => cell.textContent.trim());
+            // Устанавливаем значение "Новый" в столбец "Статус"
+            rowData[7] = 'Новый'; // 5 - индекс столбца "Статус"
+            return rowData;
+        }
+        return null; // Пропускаем скрытые строки
+    }).filter(Boolean);
+    console.log(data);
+    const worksheet = XLSX.utils.aoa_to_sheet([['Фамилия', 'Имя', 'Отчество', 'Телефон', 'Офис', 'Дата', 'Время', 'Статус'], ...data]);
+    worksheet['!cols'] = [
+        {wch: 18}, // Ширина столбца для ФИО
+        {wch: 18}, // Ширина столбца для ФИО
+        {wch: 18}, // Ширина столбца для ФИО
+        {wch: 12}, // Ширина столбца для Телефон
+        {wch: 30}, // Ширина столбца для Офис
+        {wch: 10}, // Ширина столбца для Дата
+        {wch: 10}, // Ширина столбца для Время
+        {wch: 10}, // Ширина столбца для Статус
+    ];
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Записи');
+
+    const today = new Date();
+    const fileName = 'records_' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '.xlsx';
+
+    XLSX.writeFile(workbook, fileName);
+}
